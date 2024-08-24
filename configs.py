a779426d3b02958993185ec604907e331782b1b3
arif <arif@themascoteers.com>
2024-08-24 11:51:08 +1000

    done

--- configs.py
+++ configs.py
@@ -1,5 +1,23 @@
 import datetime
 
+holidays = [
+    ["2024-01-01","2024-01-29"],
+    ["2024-03-29","2024-04-14"],
+    ["2024-06-29","2024-07-14"],
+    ["2024-09-21","2024-10-06"],
+    ["2024-01-01","2024-01-01"],
+    ["2024-01-26","2024-01-26"],
+    ["2024-03-11","2024-03-11"],
+    ["2024-03-29","2024-03-29"],
+    ["2024-03-31","2024-03-31"],
+    ["2024-04-01","2024-04-01"],
+    ["2024-04-25","2024-04-25"],
+    ["2024-06-10","2024-06-10"],
+    ["2024-09-28","2024-09-28"],
+    ["2024-11-05","2024-11-05"],
+    ["2024-12-25","2024-12-25"],
+    ["2024-12-26","2024-12-26"],
+]
 
 def get_boundary(netId):
     if netId == "CM99V122139007597":
@@ -20,3 +38,30 @@ def get_boundary_coords(netId):
 def get_date_str(timestamp):
     timestamp = datetime.datetime.fromtimestamp(timestamp / 1000.0)
     return timestamp.strftime("%Y-%m-%d %H:%M:%S")
+
+def is_drop_off(epoch_ms):
+    dt = datetime.datetime.fromtimestamp(epoch_ms / 1000.0)
+    return dt.time() >= datetime.time(8, 0) and dt.time() <= datetime.time(9, 30)
+
+def is_pick_up(epoch_ms):
+    dt = datetime.datetime.fromtimestamp(epoch_ms / 1000.0)
+    return dt.time() >= datetime.time(14, 30) and dt.time() <= datetime.time(16, 0)
+
+def is_busy_hour(timestamp):
+    return is_pick_up(timestamp) or is_drop_off(timestamp)
+
+def is_weekend(epoch_ms):
+    dt = datetime.datetime.fromtimestamp(epoch_ms / 1000.0)
+    return dt.weekday() >= 5
+
+def is_in_holiday_range(epoch_ms):
+    dt = datetime.datetime.fromtimestamp(epoch_ms / 1000.0).date()
+    for start, end in holidays:
+        start_date = datetime.datetime.strptime(start, "%Y-%m-%d").date()
+        end_date = datetime.datetime.strptime(end, "%Y-%m-%d").date()
+        if start_date <= dt <= end_date:
+            return True
+    return False
+
+def is_off_day(timestamp):
+    return is_weekend(timestamp) or is_in_holiday_range(timestamp)
---
+++ reporter.py
@@ -0,0 +1,127 @@
+import tkinter
+
+import db_handler
+import configs
+from shapely.geometry import Polygon, Point
+
+
+class Reporter:
+    def __init__(self, root):
+        self.root = root
+        self.all_windows = None
+        self.nearmisses_windows = None
+        self.nearvehicle_windows = None
+        self.networkId = None
+        self.tw = None
+
+    def get_window(self, rows, start, tw, windowId):
+        merged_frames = []
+        start_timestamp = rows[start][4]
+        end_timestamp = start_timestamp + tw
+        i = start
+        end = True
+        for i in range(start,len(rows)):
+            this_timestamp = rows[i][4]
+            if this_timestamp > end_timestamp:
+                end = False
+                break
+            merged_frames.append(rows[i])
+        next_start_index = i
+        if end:
+            next_start_index = None
+        window = {
+            "start_timestamp":start_timestamp,
+            "windowId":windowId,
+            "actors":[],
+            "hasVehicle": False,
+            "hasPed": False,
+            "hasBicycle": False,
+            "isNearMiss": False,
+            "isVehicleNearCollision": False
+        }
+        for mf in merged_frames:
+            channelId = mf[2]
+            if channelId == 0:
+                window["hasVehicle"] = True
+            elif channelId == 1:
+                window["hasPed"] = True
+            else:
+                window["hasBicycle"] = True
+            actor = {
+                "x" : mf[0],
+                "y" : mf[1],
+                "channelId": channelId,
+                "objectId": mf[3],
+            }
+            window["actors"].append(actor)
+        if window["hasVehicle"] and window["hasPed"]:
+            window["isNearMiss"] = True
+        vehicleIds = [actor["objectId"] for actor in window["actors"] if actor["channelId"] == 0]
+        vehicleIds = set(vehicleIds)
+        if len(vehicleIds) > 1:
+            window["isVehicleNearCollision"] = True
+        return window, next_start_index
+
+    def keep_crossing_only(self, rows, networkId):
+        filtered = []
+        boundary = configs.get_boundary_coords(networkId)
+        polygon = Polygon(boundary)
+        for r in rows:
+            x = r[0]
+            y = r[1]
+            point = Point(x,y)
+            if polygon.contains(point):
+                filtered.append(r)
+        return filtered
+
+
+    def get_collision(self, rows):
+        vehicles = [row for row in rows if (row[2] == 0)]
+        peds = [row for row in rows if (row[2] == 1)]
+        all = vehicles + peds
+        collision = (len(vehicles) != 0 and len(peds) != 0)
+        return collision, all
+
+
+    def plot(self, f,t,tw,n):
+        self.root.show_loading()
+        self.networkId = n
+        self.tw = tw
+        windows = []
+        self.root.canvas.delete("all")
+        rows = db_handler.get_data_by_from_to_netId(f,t,n)
+        rows = self.keep_crossing_only(rows,n)
+        start_index = 0
+        windowId = 1
+        while start_index is not None and start_index < len(rows):
+            window, start_index = self.get_window(rows, start_index, tw, windowId)
+            windows.append(window)
+            windowId = windowId + 1
+        self.all_windows = windows
+        self.nearmisses_windows = [window for window in self.all_windows if window["isNearMiss"]]
+        self.nearvehicle_windows = [window for window in self.all_windows if window["isVehicleNearCollision"]]
+        self.root.show_windows(self.all_windows, self.nearmisses_windows, self.nearvehicle_windows)
+
+    def get_window_by_id(self, windowId):
+        for window in self.all_windows:
+            if window["windowId"] == windowId:
+                return window
+        return None
+
+    def plot_window(self, n):
+        self.root.canvas.delete("all")
+        window = self.get_window_by_id(n)
+        t = f"Timestamp: Between {window['start_timestamp']} and {window['start_timestamp'] + self.tw}"
+        t = (t + f"\nDate time: Between {configs.get_date_str(window['start_timestamp'])} "
+             +f"and {configs.get_date_str(window['start_timestamp'] + self.tw)}")
+        self.root.canvas.create_text(50, 50, text=t, font=("Helvetica", 24), fill="blue", anchor=tkinter.NW)
+        self.root.canvas.create_polygon(configs.get_boundary(self.networkId), outline='yellow', fill='', width=2)
+        for actor in window["actors"]:
+            x = actor["x"]
+            y = actor["y"]
+            channelId = actor["channelId"]
+            color = 'red' if channelId == 0 else 'green' if channelId == 1 else 'blue' if channelId == 2 else 'black'
+            self.root.canvas.create_oval(x - 3, y - 3, x + 3, y + 3, fill=color, outline=color)
+            self.root.canvas.create_text(x, y-10, text=actor["objectId"], font=("Helvetica", 7), fill=color, anchor=tkinter.CENTER)
+
+
