1f64dc838b4f59282c317e64ee8b0f071a0e43f5
Arif <arif@themascoteers.com>
2024-09-08 20:45:33 +1000

    done

--- plotter2.py
+++ plotter2.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
-def plot_bar_chart(csv_file):
+def plot_bar_chart(csv_file, loc, filename):
     df = pd.read_csv(csv_file)
     df['date'] = pd.to_datetime(df['date'])
 
@@ -11,12 +12,12 @@ def plot_bar_chart(csv_file):
 
     plt.figure(figsize=(len(df) * 0.3, 6))
 
-    plt.bar(indices, df['totalNearmiss'], width=bar_width, label='Near misses',
-            color=np.where(df['is_holiday'] == 1, 'lightgray', 'blue'))
+    plt.bar(indices, df['totalNearmiss'], width=bar_width, label='Nearmiss',
+            color=np.where(df['is_holiday'] == 1, 'lightcoral', 'blue'))
     plt.bar(indices + bar_width, df['totalVehicleNearCol'], width=bar_width, label='Vehicle near misses',
-            color=np.where(df['is_holiday'] == 1, 'lightgray', 'green'))
+            color=np.where(df['is_holiday'] == 1, 'lightgreen', 'green'))
     plt.bar(indices + 2 * bar_width, df['totalJW'], width=bar_width, label='Jaywalking',
-            color=np.where(df['is_holiday'] == 1, 'lightgray', 'red'))
+            color=np.where(df['is_holiday'] == 1, 'lightpink', 'red'))
 
     plt.xlabel('Date')
     plt.ylabel('Counts')
@@ -26,6 +27,12 @@ def plot_bar_chart(csv_file):
 
     plt.legend()
     plt.tight_layout()
-    plt.savefig("plot.png")
+    filename = f"{filename}.png"
+    dest = os.path.join(loc, filename)
+    plt.savefig(dest)
 
-plot_bar_chart("report_PD.csv")
+loc = r"C:\Users\kisho\OneDrive - Web Mascot Pty Ltd\Desktop\fmll2"
+
+for f in os.listdir(loc):
+    if f.startswith("report_") and f.endswith(".csv"):
+        plot_bar_chart(os.path.join(loc, f),loc, f)
